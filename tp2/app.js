0958d3231e726762d547ef1082c0068fa880d737
Maxime Nony <maxgo.maxoue@gmail.com>
2023-11-09 18:43:07 +0100

    add end of the tp2

--- tp2/app.js
+++ tp2/app.js
@@ -11,15 +11,34 @@ addressInput.addEventListener('input', (event) => {
     }
     fetch("https://api-adresse.data.gouv.fr/search/?q=" + event.target.value).then((response) => {
         response.json().then((data) => {
+            console.log(data);
             const addressList = document.querySelector('#addresList');
+            const postalCode = document.querySelector('#cp');
+            const city = document.querySelector('#ville');
             addressList.innerHTML = '';
+            postalCode.innerHTML = '';
+            city.innerHTML = '';
             data.features.forEach((feature) => {
-                const option = new Option("", feature.properties.label);
-                addressList.appendChild(option);
-                addressInput.addEventListener("blur", () => {
-                    console.log(feature.properties.label);
-                });
+
+                const add = new Option("", feature.properties.label);
+                addressList.appendChild(add);
+                const cp = new Option("", feature.properties.postcode);
+                postalCode.appendChild(cp);
+                const ville = new Option("", feature.properties.city);
+                city.appendChild(ville);
+                
+                
+                
+                
             });
         }); 
     })
-});    
+});  
+addressInput.addEventListener("blur", () => {
+    console.log("blur");
+
+    addressInput.value = feature.properties.name;
+    postalCode.value = feature.properties.postcode;
+    city.value = feature.properties.city;
+}
+);  
